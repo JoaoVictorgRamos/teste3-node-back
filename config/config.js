e16a5869be38076b75a305db362b334dcff8e2e2
João Victor Ramos <joao.ramos@greenn.com.br>
2024-05-17 16:42:27 -0300

    fix-config

---
+++ config/config.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+console.log('MYSQLUSER:', process.env.MYSQLUSER);
+console.log('MYSQLPASSWORD:', process.env.MYSQLPASSWORD);
+console.log('MYSQLDATABASE:', process.env.MYSQLDATABASE);
+console.log('MYSQLHOST:', process.env.MYSQLHOST);
+console.log('DB_DIALECT:', process.env.DB_DIALECT);
+console.log('MYSQLPORT:', process.env.MYSQLPORT);
+
+const config = {
+  development: {
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.MYSQLPORT,
+  },
+  test: {
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.MYSQLPORT,
+  },
+  production: {
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.MYSQLPORT,
+  },
+};
+
+console.log(config);
+module.exports = config;
